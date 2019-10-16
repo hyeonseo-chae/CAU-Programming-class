7750f6678158d89d793bfd86886a6b4f85f20b1c
ckddn <eric71804122@gmail.com>
2019-10-16 12:49:44 +0900

    10월 6일(1/3)

--- practice5.c
+++ practice5.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include "stdlib.h"
-#define C 10
+#include <windows.h>
+#define C 10 // ¹Ýº¹È½¼ö  
 #define SIZE 1000
 int main() {
-	replace();
-}
-
-void replace(){
-int text[SIZE];
+	int text[SIZE];
 int target[1000];
 int replace[1000];
 char seps[]=" ";
@@ -23,33 +20,57 @@ printf("target:");
 gets(target);
 printf("replace:");
 gets(replace);
-token=strtok(text,seps);
-while(token!=NULL)
+	replaceText(text,target,replace);
+	 
+}
+
+void replaceText(char *text, char *target, char *replace) {
+	char* fin = strstr(text, target);
+	char buff[99] = "";
+	char* point = text;
+	int i,k,lenre, lenta, lente;
+	for ( i = 0; replace[i]; i++) {
+		lenre = i + 1;
+	}
+	for (i = 0; text[i]; i++) {
+		lente = i + 1;
+	}
+	for ( i = 0; target[i]; i++) {
+		lenta = i + 1;
+	}
+	 i = 0;
+	while (fin != NULL) {
+		int d = fin - text;
+		char temp[99] = "";
+		for (i = 0; i < fin - text; i++) {
+			temp[i] = text[i];
+		}
+		for ( k = 0; k < lenre ; k++,i++) {
+			temp[i] = replace[k];
+		}
+		strcat(buff, temp);
+		text += d + lenta;
+
+		fin = strstr(text, target);
+	}
+	strcpy(point, buff);
+	strcat(point, " ");
+	system("cls"); 
+	scroll(point,C);
+}
+
+void CursorView(char show)//Ä¿¼­¼û±â±â 	
 {
-    strcpy(b[count],token);
-    count++;
-token=strtok(NULL,seps);
+    HANDLE hConsole;
+    CONSOLE_CURSOR_INFO ConsoleCursor;
+
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    ConsoleCursor.bVisible = show;
+    ConsoleCursor.dwSize = 1;
+
+    SetConsoleCursorInfo(hConsole , &ConsoleCursor);
 }
-for(j=0;j<count;j++)
-{
-if(strcmp(b[j],target)==0)
-    {strcpy(b[j],replace);
-break;}
-}
-
-for(j=0;j<count;j++)
-{
-
-strcat(kk,b[j]);
-strcat(kk," ");
-}
-scroll(kk,C);
-}
-
-
-
-
-
 
 void scroll(char *str,int n){
 	printf("\n");
@@ -62,7 +83,7 @@ void scroll(char *str,int n){
   
         for(i=0;i<len;i++)
         {printf("%c", str[i]);} 
-		system("cls"); Sleep(10);
+		CursorView(0); Sleep(100); system("cls"); 
         for(i=0;i<len;i++)
         {
             if(i==0)
