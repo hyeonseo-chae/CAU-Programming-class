3be2e9dd34965d661ac6c9344020422fada21c8c
MrKatsee <eric71804122@gmail.com>
2019-10-16 14:34:17 +0900

    커밋 수정

--- practice5.c
+++ practice5.c
@@ -6,46 +6,46 @@
 #define SIZE 1000
 int main() {
 	int text[SIZE];
-int target[1000];
-int replace[1000];
-char seps[]=" ";
-int b[10][20];
-char *token;
-int kk[1000];
-int j,count;
-count=0;
-printf("text:");
-gets(text);
-printf("target:");
-gets(target);
-printf("replace:");
-gets(replace);
-	replaceText(text,target,replace);
-	 
+	int target[1000];
+	int replace[1000];
+	char seps[] = " ";
+	int b[10][20];
+	char* token;
+	int kk[1000];
+	int j, count;
+	count = 0;
+	printf("text:");
+	gets(text);
+	printf("target:");
+	gets(target);
+	printf("replace:");
+	gets(replace);
+	replaceText(text, target, replace);
+
 }
 
-void replaceText(char *text, char *target, char *replace) {
+void replaceText(char* text, char* target, char* replace) {
 	char* fin = strstr(text, target);
 	char buff[99] = "";
 	char* point = text;
-	int i,k,lenre, lenta, lente;
-	for ( i = 0; replace[i]; i++) {
+	int i, k, lenre, lenta, lente;
+	for (i = 0; replace[i]; i++) {
 		lenre = i + 1;
 	}
 	for (i = 0; text[i]; i++) {
 		lente = i + 1;
 	}
-	for ( i = 0; target[i]; i++) {
+	for (i = 0; target[i]; i++) {
 		lenta = i + 1;
 	}
-	 i = 0;
+	i = 0;
 	while (fin != NULL) {
 		int d = fin - text;
 		char temp[99] = "";
 		for (i = 0; i < fin - text; i++) {
 			temp[i] = text[i];
 		}
-		for ( k = 0; k < lenre ; k++,i++) {
+		for (k = 0; k < lenre; k++, i++) {
 			temp[i] = replace[k];
 		}
 		strcat(buff, temp);
@@ -55,50 +55,54 @@ void replaceText(char *text, char *target, char *replace) {
 	}
 	strcpy(point, buff);
 	strcat(point, " ");
-	system("cls"); 
-	scroll(point,C);
+	system("cls");
+	scroll(point, C);
 }
 
 void CursorView(char show)//Ä¿¼­¼û±â±â 	
 {
-    HANDLE hConsole;
-    CONSOLE_CURSOR_INFO ConsoleCursor;
+	HANDLE hConsole;
+	CONSOLE_CURSOR_INFO ConsoleCursor;
 
-    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    ConsoleCursor.bVisible = show;
-    ConsoleCursor.dwSize = 1;
+	ConsoleCursor.bVisible = show;
+	ConsoleCursor.dwSize = 1;
 
-    SetConsoleCursorInfo(hConsole , &ConsoleCursor);
+	SetConsoleCursorInfo(hConsole, &ConsoleCursor);
 }
 
-void scroll(char *str,int n){
+void scroll(char* str, int n) {
 	printf("\n");
-	int count=0,i,temp,k;
+	int count = 0, i, temp, k;
 	int len = strlen(str);
-	for(k=0;k<n;k++){
-	
-	while(count<=len)
-    {
-  
-        for(i=0;i<len;i++)
-        {printf("%c", str[i]);} 
-		CursorView(0); Sleep(100); system("cls"); 
-        for(i=0;i<len;i++)
-        {
-            if(i==0)
-                {temp = str[len];
-                str[len]=str[i];
-                str[i]=str[i+1];}
-            else if(i==len-1)
-                str[i]=temp;
-        str[i]=str[i+1];
-        }
-    count++;
-    }
-    count = 0;
-}
-	
+	for (k = 0; k < n; k++) {
+
+		while (count <= len)
+		{
+
+			for (i = 0; i < len; i++)
+			{
+				printf("%c", str[i]);
+			}
+			CursorView(0); Sleep(100); system("cls");
+			for (i = 0; i < len; i++)
+			{
+				if (i == 0)
+				{
+					temp = str[len];
+					str[len] = str[i];
+					str[i] = str[i + 1];
+				}
+				else if (i == len - 1)
+					str[i] = temp;
+				str[i] = str[i + 1];
+			}
+			count++;
+		}
+		count = 0;
+	}
+
 }
 
 
