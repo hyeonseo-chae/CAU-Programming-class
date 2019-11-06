6f1f9e617043288cca4aebd6439f716468b8e122
hyeonseo-chae <eric71804122@gmail.com>
2019-11-06 16:27:51 +0900

    qsort 까지 끝

--- practice6.c
+++ practice6.c
@@ -11,16 +11,26 @@ int year, month, day;
 
 typedef struct struct_person {
 char name[MAX_STR];
-<<<<<<< HEAD
 int telephone; 
 Sdate birthday;
 } Sperson;
 
 int i = 0;
-Sperson *p[100];
+Sperson p[100];
 int pnumber=0;
 
-void main(){
+
+void one();
+void print();
+void three();
+void four();
+void five();
+void six();
+void seven();
+int compare(void *a, void *b);
+void input();
+
+void main(){ 
 	fp = fopen("1.txt", "w+");
 
     if (fp==0){
@@ -30,7 +40,7 @@ void main(){
     }
 
 	while(1){	
-		printf("\n1. Add a person\n");
+	printf("\n1. Add a person\n");
 	printf("2. Print the list\n");
 	printf("3. Name search\n");
 	printf("4. Birthday this month!\n");
@@ -53,50 +63,11 @@ void main(){
 		case 0: exit(0); fclose(fp); break;
 	}
 }	
-=======
-char telephone[MAX_STR];
-Sdate birthday;
-} Sperson;
-
-void one(Sperson *a1);
-void two(Sperson *a1);
-void three(Sperson *a1);
-void four(Sperson *a1);
-void input(Sperson *a1);
-void print(Sperson *a1);
-void check(Sperson *a1);
-
-int main(){
-   Sperson a1;
-   printf("1. Add a person\n");
-   printf("2. Print the list\n");
-   printf("3. Name search\n");
-   printf("4. Birthday this month!\n");
-   printf("0. Quit the program\n");
-   while(1){
-   	printf("------\n");
-   printf("Enter the command: ");
-   int a;
-   scanf("%d",&a);
-   printf("------\n");
-   fflush(stdin);
-   switch (a){
-      case 1: one(&a1); break;
-      case 2: two(&a1); break;
-      case 3: three(&a1); break;
-      case 4: four(&a1); break;
-      case 0: exit(0); break;
-      default: printf("Wrong command. Enter again\n"); continue;
-
-   }
-}
->>>>>>> baea3bcace7c7fac18cd1315993ed56488b2ef56
 
 
 
 }
 
-<<<<<<< HEAD
 void one() {
 	
 	input();
@@ -115,15 +86,15 @@ void three(){
 
     for (a = 0; a <= (i-1); a++) {
 
-        got = strcmp(p[a]->name, getname);
+        got = strcmp(p[a].name, getname);
 
         if (got == 0) {
 
             printf("[%d] %s",a, getname);
 
-            printf("0%d",p[a]->telephone);
+            printf("0%d",p[a].telephone);
 
-            printf("%4d.%2d.%2d.\n", p[a]->birthday.year, p[a]->birthday.month, p[a]->birthday.day);
+            printf("%4d.%2d.%2d.\n", p[a].birthday.year, p[a].birthday.month, p[a].birthday.day);
 
         }
 
@@ -144,10 +115,10 @@ void four(){
     int noone=0;
         for ( m = 0; m <=i-1; m++) {
 
-        if ((p[m]->birthday.month) == (tm.tm_mon+1)) {
+        if ((p[m].birthday.month) == (tm.tm_mon+1)) {
         	printf("Happy Birthdays!!!\n");
 
-            printf("[%d] %s, 0%d, %4d.%2d.%2d.\n", m,p[m]->name,p[m]->telephone,p[m]->birthday.year, p[m]->birthday.month, p[m]->birthday.day);
+            printf("[%d] %s, 0%d, %4d.%2d.%2d.\n", m,p[m].name,p[m].telephone,p[m].birthday.year, p[m].birthday.month, p[m].birthday.day);
 
         }
 
@@ -173,7 +144,9 @@ void four(){
 int compare(void *a, void *b)    
 
 {
-    return strcmp((char*)a,(char*)b);
+	Sperson arg1 = *(Sperson*)a;
+	Sperson arg2 = *(Sperson*)b;
+    return strcmp(arg1.name,arg2.name);
 }
 
 
@@ -203,7 +176,7 @@ void six(){
 
     for (b=0; b<i; b++){
 
-    fprintf(fp, "%s\n0%d\n%04d%02d%02d\n",p[b]->name,p[b]->telephone, p[b]->birthday.year,p[b]->birthday.month, p[b]->birthday.day);
+    fprintf(fp, "%s\n0%d\n%04d%02d%02d\n",p[b].name,p[b].telephone, p[b].birthday.year,p[b].birthday.month, p[b].birthday.day);
 
     }
 
@@ -219,7 +192,7 @@ void seven(){
 
     for(f=0; f<pnumber; f++){
 
-        fscanf(fp, "%s\n0%d\n%04d%02d%02d\n", &p[f]->name, &p[f]->telephone, &p[f]->birthday.year, &p[f]->birthday.month, &p[f]->birthday.day);
+        fscanf(fp, "%s\n0%d\n%04d%02d%02d\n", &p[f].name, &p[f].telephone, &p[f].birthday.year, &p[f].birthday.month, &p[f].birthday.day);
 
         }
 
@@ -230,7 +203,6 @@ void seven(){
 
 
 void input(){
-	 p[i] = malloc(sizeof(Sperson));
 
     
 
@@ -243,20 +215,20 @@ void input(){
     fflush(stdin);
 	printf("Enter the name:");
 	gets(str);
-	strcpy(p[i]->name, str);
+	strcpy(p[i].name, str);
     fflush(stdin);
     printf("Enter the phone number:");
 	scanf("%d",&num);
-	p[i]->telephone = num;
+	p[i].telephone = num;
 
     printf("Enter the birthday (yyyymmdd):"); //생년월일 8자리만 인식 가 능  
 	scanf("%4d",&year);
-	p[i]->birthday.year = year;
+	p[i].birthday.year = year;
 	
 	scanf("%2d",&month);
-	p[i]->birthday.month = month;
+	p[i].birthday.month = month;
 	scanf("%2d",&day);
-   p[i]->birthday.day = day;
+   p[i].birthday.day = day;
   
     printf("\n");
 
@@ -273,64 +245,17 @@ void input(){
 
 	
 	
-=======
-void one(Sperson *a1) {
-   input(a1);
-}
-
-void two(Sperson *a1){
-   check(a1);
-}
-
-void three(Sperson *a1){
-   char kk[20];
-   scanf("%s",kk);
-   char * ptr = strstr((a1->name),kk);
-   if(ptr != NULL){
-   	printf("------\n");
-      printf("%s, %s, %4d.%2d.%2d.\n",a1->name,a1->telephone,a1->birthday.year,a1->birthday.month,a1->birthday.day);
-   }
-
-}
-
-void four(Sperson *a1){
-   time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-
-   if((a1->birthday.month )== (tm.tm_mon+1)){
-   	printf("------\n");
-      printf("Happy Birthdays!!!\n");
-         printf("%s, %s, %4d.%2d.%2d.\n",a1->name,a1->telephone,a1->birthday.year,a1->birthday.month,a1->birthday.day);
-   }
-}
-void input(Sperson *a1){
-   fflush(stdin);
-   printf("Enter the name:");
-   gets(a1->name);
-
-   printf("Enter the phone number:");
-   gets(a1->telephone);
-
-    printf("Enter the birthday (yyyymmdd):"); //생년월일 8자리만 인식 가 능
-   scanf("%4d",&a1->birthday.year);
-
-   scanf("%2d",&a1->birthday.month);
-
-   scanf("%2d",&a1->birthday.day);
-
->>>>>>> baea3bcace7c7fac18cd1315993ed56488b2ef56
 
 
 void print() {
 	int k;
-	 for (k = 0; k <= (i-1); k++) {
-	 	if(p[k]->birthday.year>=0 && p[k]->birthday.month>=1 && p[k]->birthday.month<=12 && p[k]->birthday.day>=1 && p[k]->birthday.day<=31){
-		printf("[%d] Name: %s\n", k + 1, p[k]->name);
+	 for (k = 0; k <i; k++) {
+	 	if(p[k].birthday.year>=0 && p[k].birthday.month>=1 && p[k].birthday.month<=12 && p[k].birthday.day>=1 && p[k].birthday.day<=31){
+		printf("[%d] Name: %s\n", k + 1, p[k].name);
 
-        printf("telephone: 0%d\n", p[k]->telephone);
+        printf("telephone: 0%d\n", p[k].telephone);
 
-<<<<<<< HEAD
-        printf("Birthday: %d.%d.%d\n", (p[k]->birthday.year),(p[k]->birthday.month), (p[k]->birthday.day));
+        printf("Birthday: %d.%d.%d\n", (p[k].birthday.year),(p[k].birthday.month), (p[k].birthday.day));
 	}
 	
 	else{
@@ -342,21 +267,4 @@ void print() {
     }
 
 
-=======
-void check(Sperson *a1){
-   if(a1->birthday.year>=0 && a1->birthday.month>=1 && a1->birthday.month<=12 && a1->birthday.day>=1 && a1->birthday.day<=31){
-      print(a1);
-   }
-   else{
-      printf("생년월일이 잘못 입력되었습니다.\n");
-   }
 }
-
-void print(Sperson *a1) {
-	printf("------\n");
-   printf("Name: %s\n",a1->name);
-   printf("Telephone: %s\n",a1->telephone);//아직 전화번호 - 출력 안 됨
-   printf("Birthday: %4d.%2d.%2d.\n",a1->birthday.year,a1->birthday.month,a1->birthday.day);
->>>>>>> baea3bcace7c7fac18cd1315993ed56488b2ef56
-}
-
