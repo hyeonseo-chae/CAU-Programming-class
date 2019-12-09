856f5c323c7c885acad86a7e3b09a73281e1cb2d
unknown <eric71804122@gmail.com>
2019-12-09 12:54:33 +0900

    dddd

---
+++ Project3111/Project3/hangdraw.cpp
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include "hangdraw.h"
+
+const char *map[] = {
+	"    ----------     ",
+	"    |              ",
+	"    |              ",
+	"    |              ",
+	"    |              ",
+	"    |              ",
+	"    |              ",
+	"    |              ",
+	"____|______________",
+		"",
+	//	"Word : ",
+	//	"Misses : ",
+	//	"Guess : ",
+};
+int nmap = sizeof(map) / sizeof(map[0]);
+
+const char *hangs[] = {
+	" |",
+	" @",
+	"/", "|", ">",
+	" | ",
+	"/", " )"
+};
+int nhangs = sizeof(hangs) / sizeof(hangs[0]);
+
+int hangpos[][2] = {
+					{ 9, 1 },
+					{ 9, 2 },
+					{ 9, 3 }, { 10, 3 }, { 11, 3 },
+					{ 9, 4 },
+					{ 9, 5 }, { 10, 5 }
+};
+
+const char *winPic[] = {
+	"(O/",
+	" | ",
+	"/ >"
+};
+int nwinPic = sizeof(winPic) / sizeof(winPic[0]);
+
+
+void setColor(WORD color)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+bool getColor(WORD &ret) {
+	CONSOLE_SCREEN_BUFFER_INFO info;
+	if (!GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info))
+		return false;
+	ret = info.wAttributes;
+	return true;
+}
+
+void clrscr()
+{
+	system("cls");
+}
+
+void gotoxy(int x, int y) //콘솔 화면 특정 위치로 이동
+{
+	COORD Pos = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
+
+void drawPic(const char *pic[], int npic, int x, int y)
+{
+	for (int i = 0; i < npic; i++) {
+		gotoxy(x, y + i);
+		printf("%s", pic[i]);
+	}
+}
+
+
+void drawHangman(int mode)
+{
+	switch (mode) {
+	case 0:
+		drawPic(map, nmap, WINX, WINY);
+		break;
+	case 1: case 2: case 3: case 4:
+	case 5: case 6: case 7: case 8:
+		gotoxy(WINX + hangpos[mode - 1][0], WINY + hangpos[mode - 1][1]);
+		printf("%s", hangs[mode - 1]);
+		if (mode == NCHANCE) {
+			gotoxy(WINX + 6, WINY + 6);
+			WORD prevColor;
+			getColor(prevColor);
+			setColor(FOREGROUND_RED | FOREGROUND_INTENSITY);
+
+			printf("You Die!!");
+			setColor(prevColor);
+		}
+		break;
+	case Win:
+		drawPic(map, nmap, WINX, WINY);
+		gotoxy(WINX + 6, WINY + 3);
+		printf("You Win !!");
+		drawPic(winPic, nwinPic, WINX + 9, WINY + 5);
+		break;
+	default:
+		break;
+	}
+	gotoxy(WINX + 8, WINY + 12);
+}
+
+/// drawHangman 사용법 예제
+#include <conio.h>
+
+void drawHangmanTest()
+{
+	clrscr();
+	drawHangman(0);
+	gotoxy(TEXTX, TEXTY);
+	printf("Press any key to start");
+	_getch();
+	for (int i = 1; i <= NCHANCE; i++) {
+		drawHangman(i);
+		gotoxy(TEXTX, TEXTY);
+		printf("You missed %d letters     ", i);
+		_getch();
+	}
+	drawHangman(Win);
+	gotoxy(TEXTX, TEXTY);
+	printf("Press any key to continue...");
+	_getch();
+}
---
+++ Project3111/Project3/hangdraw.h
@@ -0,0 +1,28 @@
+#ifndef _HANGDRAW_H
+#define _HANGDRAW_H
+
+#include<windows.h>
+
+#define WINX  5		// 그림을 그리는 맨 왼쪽 위치
+#define WINY  2		// 그림을 그리는 맨 위쪽 위치
+#define TEXTX  WINX			// 텍스트를 그리는 맨 왼쪽 위치
+#define TEXTY  (WINY+10)	// 텍스트를 그리는 맨 위쪽 위치
+#define Win   100	// 이겼을 때의 모드 - drawHangman의 입력 파라미터
+#define NCHANCE 8	// 글자를 추측할 수 있는 기회의 수
+
+// Functions
+void clrscr(); // 화면을 지움
+void gotoxy(int x, int y); //콘솔 화면의 (x, y) 위치로 이동
+void setColor(WORD color); //폰트 및 배경 색 지정
+
+// hangman을 그림
+// mode 값 0: 전체 틀을 그림
+// mode 값 1~7: 매달리는 부위를 하나씩 그림
+// mode 값 8 == NCHANCE: 게임에 졌을 때의 화면 출력
+// mode 값 Win : 게임에 이겼을 때의 화면 출력
+void drawHangman(int mode);
+
+void drawHangmanTest(); // drawHangman 사용 예제
+
+
+#endif
